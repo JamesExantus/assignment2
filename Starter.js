47e24e8fc631fb17085bee6152c52418363c8899
jamesexantus <jamesexantus@ymail.com>
2023-06-27 23:33:41 -0400

    fourth

--- Starter.js
+++ Starter.js
@@ -1,39 +1,38 @@
 
-// list of  callback functions 
+// list of  callback functions  and objects
 function myEachCallBack(elem,a,arr){
     elem = arr[a];
     console.log(arr[a]);
 }
 
 function myMapCallBack(elem,a,arr){
-    elem = arr[a];
-    console.log(a+1,arr[a]);
+    elem = arr[a] * 2;
+
+    return elem;
 }
+
 function myFilterCallBack(elem, a, arr, testing){
         elem = arr[a];
-        if(elem.length > testing){
-            console.log(elem);
+        if(elem.length >= testing){
+            return true;
         }
+            return false;
 }
 
 function mySomeCallBack(elem, a, arr){
-    if(arr[a] >=21 ){
-        console.log(true);
+    if(arr[a] >= 21 ){
         return true;
     }
     else {
-        console.log(false);
         return false;
     }
 }
 
 function myEveryCallBack(arr,a){
-    if(arr[a] < 24){
-        console.log(true);
+    if(arr[a] >= 21){
         return true;
     }
     else{
-        console.log(false);
         return false;
     }
 }
@@ -48,11 +47,22 @@ function myReduceCallBack(accu, currv, arr ){
     
 }
 
+const myKeyObjects = {
+    age : 21,
+    birthday : "december",
+    placement : "first"
+};
+
 //end of list
 
-//My Each function.
-const orderOf = ["first","second","third"];
 
+// arrays for functions.
+const shortenedMonth = ["jan", "feb", "march", "apr", "may", "june", "jul", "aug", "sep", "oct", "nov", "dec"];
+const orderOf = ["first","second","third"];
+const overTwentyOne = [18, 19, 20, 21, 22, 23];
+
+
+//My Each function.
 function myEach(nums,cb) {
     for(let i = 0; i< nums.length; i++){
         cb(nums[i],i,nums);
@@ -60,45 +70,51 @@ function myEach(nums,cb) {
 }
     myEach(orderOf,myEachCallBack);
     
-
 // My Map
 const kyArray = [10,20,30];
 function myMap(nums,cb){
+    let newArray = []
     for(let i = 0; i <nums.length; i++){
-       cb(nums[i],i,nums);
+       newArray.push(cb(nums[i],i,nums));
     }
+    return newArray;
+
 }
+console.log(myMap(kyArray,myMapCallBack));
 myMap(kyArray,myMapCallBack);
 
 //My filter
-const shortenedMonth = ["jan", "feb", "mar", "apr", "may", "june", "jul", "aug", "sep", "oct", "nov", "dec"];
-let criteria = 3;
 
 function myFilter(nums,cb,testnum)
 {
+    let newArray = [];
     for(let i = 0; i<nums.length; i++){
-        cb(nums[i],i,nums,testnum);
+       if(cb(nums[i],i,nums,testnum) == true){
+        newArray.push(nums[i]);
+       }
     }
-    return nums;
+    return newArray;
 }
+console.log(myFilter(shortenedMonth,myFilterCallBack,4));
 myFilter(shortenedMonth,myFilterCallBack,4);
 
 //my Some
-const overTwentyOne = [18, 19, 20, 21, 22, 23];
-
 function mySome(nums, cb){
     for(let i = 0; i<nums.length; i++){
-        cb(nums[i],i,nums);
+        if(cb(nums[i],i,nums)){
+            return true;
+        };
     }
+    return false;
 }
 mySome(overTwentyOne,mySomeCallBack);
 
 //my Every
 function myEvery(nums,cb){
     for(let i = 0; i < nums.length; i++){
-      cb(nums[i],i,nums);
-
+      if(cb(nums[i],i,nums) == false);
     }
+    return true
 }
 myEvery(overTwentyOne,myEveryCallBack);
 
@@ -108,15 +124,11 @@ function myReduce(nums, cb) {
     let currvalue = nums[0];
         cb(accumulator, currvalue,nums);
 }
-
 myReduce(overTwentyOne, myReduceCallBack);
-
-/*
-    The entire browser crashes when these functions are ran. 
 
 //my includes
 function myincludes(nums,target) {
-    for(let i = 0; nums.length; i++){
+    for(let i = 0; i < nums.length; i++){
         if(nums[i] == target){
             console.log(true);
             return true;
@@ -125,11 +137,8 @@ function myincludes(nums,target) {
 }
 myincludes(overTwentyOne,21);
 
-
 //myIndexOf
-*/
 function myIndexOf(nums,target){
-    
     let temp = -1;
     for(let i = 0; i < nums.length; i++){
         if(nums[i]== target) {
@@ -145,35 +154,51 @@ myIndexOf(overTwentyOne,21);
 
 // my push
 function myPush(elementn,nums){
-    
     let oldsize = nums.length;
    let size = nums.length * 2;
    let newarr = [size];
-
     for(let i = 0; i < nums.length; i++){
         newarr[i] = nums[i];
-       
     }
     newarr[oldsize] = elementn;
     nums = newarr;
-    
-
     return nums;
 }
 myPush(40, overTwentyOne);
 
 //my unshift
-function myUnshift(){
+function myUnshift(nums, target){
 
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] == target){
+            console.log(nums[i]);
+            return nums[i];
+        }
+    }
+    return -1;
 }
+    myUnshift(overTwentyOne, 22);
+
 
 //grab keys
-
-function grabKeys(){
-
+function grabKeys(keyobj){
+    let keytxt = " ";
+    for(x in myKeyObjects ){
+        keytxt += x;
+        console.log(keytxt);
+    }
+    console.log(keytxt);
+    return keytxt;
 }
+grabKeys(myKeyObjects);
 
-//grag values
-function grabValues(){
-
+//grab values
+function grabValues(keyobj){
+    let txt = " ";
+    for( x in myKeyObjects ){ 
+        txt += myKeyObjects[x];   
+        console.log(txt); 
+    }
+    return txt;
 }
+grabValues(myKeyObjects);
