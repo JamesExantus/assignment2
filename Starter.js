eb6062492e45667d88aa3c1471fa0a54d9cb5272
jamesexantus <jamesexantus@ymail.com>
2023-06-28 22:03:31 -0400

    final

--- Starter.js
+++ Starter.js
@@ -53,13 +53,24 @@ const myKeyObjects = {
     placement : "first"
 };
 
-//end of list
+//end of list of callbacks
 
 
 // arrays for functions.
 const shortenedMonth = ["jan", "feb", "march", "apr", "may", "june", "jul", "aug", "sep", "oct", "nov", "dec"];
 const orderOf = ["first","second","third"];
 const overTwentyOne = [18, 19, 20, 21, 22, 23];
+const sumExample = [1,2,3,4,5,6,7,8,9,10];
+const list1 = {
+    value: 1, rest: { value: 2, rest: {  value: 3,  rest: null  }   }
+};
+
+const list2 = {
+    value: 1, rest: { value: 2, rest: {  value: 3,  rest: null  }   }
+};
+
+const newlist = ['first', 'second', 'third'];
+
 
 
 //My Each function.
@@ -171,8 +182,8 @@ function myUnshift(nums, target){
 
     for(let i = 0; i < nums.length; i++){
         if(nums[i] == target){
-            console.log(nums[i]);
-            return nums[i];
+            console.log(i);
+            return i;
         }
     }
     return -1;
@@ -182,23 +193,158 @@ function myUnshift(nums, target){
 
 //grab keys
 function grabKeys(keyobj){
+    let newArray = [];
     let keytxt = " ";
     for(x in myKeyObjects ){
-        keytxt += x;
-        console.log(keytxt);
+        newArray.push(x);
+      
     }
-    console.log(keytxt);
-    return keytxt;
+    console.log(newArray);
+    return newArray
 }
 grabKeys(myKeyObjects);
 
 //grab values
 function grabValues(keyobj){
     let txt = " ";
+    let newArray = [];
     for( x in myKeyObjects ){ 
-        txt += myKeyObjects[x];   
-        console.log(txt); 
+    
+        newArray.push(myKeyObjects[x]); 
     }
-    return txt;
+    console.log(newArray);
+    return newArray;
 }
 grabValues(myKeyObjects);
+
+//sum of a range
+
+function range(start, end){
+    let newArray = [];
+    let left = start;
+    let numofelements = end - start;
+
+    for( let i = 0 ; i <= numofelements; i++){
+        newArray[i] = left;
+        left++;
+        console.log(newArray[i]);
+    }
+    return newArray;
+}
+
+range(5,10);
+
+function sum(nums){
+    let accumulator = 0;
+    for(let i = 0; i < nums.length; i++){
+        accumulator += nums[i]; 
+    }
+    console.log(accumulator);
+    return accumulator;
+}
+
+sum(sumExample);
+
+//reverse array and reverse array in place
+
+function reverseArray(nums){
+    let newArray = [];
+    let right = nums.length-1;
+    for(let left = 0; left < nums.length; left++){
+        newArray[left] = nums[right];
+        right--;
+    }
+    console.log(newArray);
+    return newArray;
+}
+
+reverseArray(overTwentyOne);
+
+function reverseArrayinPlace(nums){
+    let right = nums.length;
+    
+    for(let left = 0; left <= nums.length; left++){
+        while(right > left){
+        nums[left] = nums[right];
+        right --;
+        }
+    }
+    console.log(nums);
+    return nums;
+}
+reverseArrayinPlace(overTwentyOne);
+
+//a List
+function arrayToList(num1, num2, num3){
+    let list = {
+        value: num1,
+        rest: {
+            value: num2,
+            rest: {
+                value: num3,
+                rest: null
+            }
+        }
+    };
+
+}
+
+function listToArray(atlList){
+let newArray = [];
+    for(let i = 0; i != null; i++){
+        newArray[i] = atlList.indexof(i);
+    }
+    while (atlList != null){
+
+    } 
+
+}
+
+
+function prepend(elem, atlList){
+    let newList = [elem, ...atlList];
+
+    return newList;
+
+}
+
+function nth(atlList, target){
+    if (target < 0 || target >= atlList.length) {
+        return undefined;
+      }
+    
+      return atlList[target];
+     
+}
+nth(newlist, 0);
+
+//deep equal function
+
+function deepEqual(val1, val2) {
+    if (val1 === val2) {
+        return true;
+      }
+    
+      if (typeof val1 !== 'object' || typeof val2 !== 'object' || val1 === null || val2 === null) {
+        return false;
+      }
+    
+      const keysA = Object.keys(val1);
+      const keysB = Object.keys(val2);
+    
+      if (keysA.length !== keysB.length) {
+        return false;
+      }
+    
+      for (let key of keysA) {
+        if (!keysB.includes(key) || !deepEqual(val1[key], val2[key])) {
+          return false;
+        }
+      }
+    
+      return true;
+    
+    }
+
+console.log(deepEqual(list1, list2));
+deepEqual(list1,list2);
